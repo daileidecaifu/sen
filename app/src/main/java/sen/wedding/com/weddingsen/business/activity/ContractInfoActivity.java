ec8b39c9d2d71dbe0bb4ffe9b81f3814d9548b90
daileidecaifu <daileidecaifu@hotmail.com>
2017-05-04 00:05:26 +0800

    1、增加图片压缩

--- app/src/main/java/sen/wedding/com/weddingsen/business/activity/ContractInfoActivity.java
+++ app/src/main/java/sen/wedding/com/weddingsen/business/activity/ContractInfoActivity.java
@@ -2,6 +2,7 @@ package sen.wedding.com.weddingsen.business.activity;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -9,6 +10,10 @@ import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +23,7 @@ import sen.wedding.com.weddingsen.R;
 import sen.wedding.com.weddingsen.base.BaseActivity;
 import sen.wedding.com.weddingsen.business.adapter.PhotoAdapter;
 import sen.wedding.com.weddingsen.business.utils.RecyclerItemClickListener;
+import sen.wedding.com.weddingsen.component.compress.CompressHelper;
 import sen.wedding.com.weddingsen.databinding.ContractInfoBinding;
 
 /**
@@ -66,7 +72,9 @@ public class ContractInfoActivity extends BaseActivity implements View.OnClickLi
     public void onClick(View v) {
 
     }
-    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK &&
@@ -83,6 +91,38 @@ public class ContractInfoActivity extends BaseActivity implements View.OnClickLi
                 selectedPhotos.addAll(photos);
             }
             photoAdapter.notifyDataSetChanged();
+
+            File oldFile = new File(selectedPhotos.get(0));
+            Glide.with(this)
+                    .load(Uri.fromFile(oldFile))
+                    .centerCrop()
+                    .thumbnail(0.1f)
+                    .placeholder(R.drawable.__picker_ic_photo_black_48dp)
+                    .error(R.drawable.__picker_ic_broken_image_black_48dp)
+                    .into(binding.imageView1);
+            binding.tvOld.setText(String.format("Size : %s", getReadableFileSize(oldFile.length())));
+
+
+            File newFile = CompressHelper.getDefault(getApplicationContext()).compressToFile(oldFile);
+            Glide.with(this)
+                    .load(Uri.fromFile(newFile))
+                    .centerCrop()
+                    .thumbnail(0.1f)
+                    .placeholder(R.drawable.__picker_ic_photo_black_48dp)
+                    .error(R.drawable.__picker_ic_broken_image_black_48dp)
+                    .into(binding.imageView2);
+            binding.tvNew.setText(String.format("Size : %s", getReadableFileSize(newFile.length())));
+
         }
+    }
+
+
+    public String getReadableFileSize(long size) {
+        if (size <= 0) {
+            return "0";
+        }
+        final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 }
